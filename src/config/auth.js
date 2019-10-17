52f8423a502aaeb58d83b71568dbc9254dea52b7
Gilmar Silva <gilmarheavensilva@hotmail.com>
2019-10-17 15:33:10 -0300

    add new configs

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9aafad407f857ef19a3bee004a7df4a4',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const router = new Router();
 
 router.post('/users', UserController.store);
+router.post('/sessions', SessionController.store);
+
+router.use(authMiddleware);
+
+router.put('/users', UserController.update);
 
 
 export default router;
